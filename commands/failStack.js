fa0234c2b1f95cfee0c848f7fbe004f26c9a3181
staryg <micke.k4@gmail.com>
2017-07-03 15:49:31 +0200

    formatted based on jshinter and changed a little bit of code.
    I haven't really fixed anything yet, still trying to find solutions.

--- commands/failStack.js
+++ commands/failStack.js
@@ -1,38 +1,30 @@
 'use strict';
-const Discord = require('discord.js');
-const client = new Discord.Client();
+var Discord = require('discord.js');
+var client = new Discord.Client();
 
-const sheetFunction = require('../sheetFunction.js');
-const config = require('../../json/config.json');
-const sheetData = require('../../json/sheetData.json')
+var config = require('../../json/config.json');
+var sheetData = require('../../json/sheetData.json');
 
-let args = message.content.split(' ').slice(1);
-// E = Enhance, R = repair
-let itemName = args[0];
-let enhanceWanted = args[1];
+var fs = {
+	help: config.help.failstack,
+	func: (client, message, args) => {
+		// E = Enhance, R = Repair/replace, D = re-enhance (degrade part),
+		var itemName = args[0];
+		var enhanceWanted = args[1];
 
-debugger;
-var sheetDataFilled = sheetData.values[[0]];
-var sheetItemName = sheetDataFilled.indexOf(itemName);
-if (sheetItemName === -1) {
-	message.reply('Command not formatted correctly.')
-		.then(msg => console.log())
-		.catch(console.error);
-} else {
-	var
-	var r = resp.fs[0];
-	var r2 = resp.fs[1];
-	var r3 = resp.fs[2];
-	var r4 = resp.fs[3];
-	var r5 = resp.fs[4];
-	var sheetFailstack = sheetDataFilled.indexOf[enhanceWanted];
+		var a = sheetData.values[[0]];
+		var sheetItemName = a.indexOf(itemName);
+		if (sheetItemName === -1) {
 
+			message.reply('Command not formatted correctly.')
+				.then(msg => console.log())
+				.catch(console.error);
+		} else {
 
-	message.reply(r + sheetFailstack + ' ' + sheetItemName + r2 + r3 + r4 + r5 + d)
-		.then(msg => console.log(`${msg}`))
-		.catch(console.error);
-}
-
-let fsCmd = {
-	help: config.help.fs
-}
+			var sheetFailstack = a.indexOf(enhanceWanted);
+			message.reply()
+				.then(msg => console.log(`${msg}`))
+				.catch(console.error);
+		}
+	}
+};
